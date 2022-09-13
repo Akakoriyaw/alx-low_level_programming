5bcb750c32e68ef1cef8e36cf91b6e29732b8606
Akakoriyaw <akakoriyaw@gmail.com>
2022-09-13 14:32:20 -0700

    task12

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -10,9 +10,9 @@ int print_sign(int n);
 int _abs(int);
 int print_last_digit(int);
 void jack_bauer(void);
-void times_table(void);
 int add(int a, int b);
 void print_to_98(int a);
 void num(int r, int n);
+void print_times_table(int n);
 
 #endif
