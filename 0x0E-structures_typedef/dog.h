faad3d7a7d875829eb36d4626a5c47a0364b3db6
Akakoriyaw <akakoriyaw@gmail.com>
2022-10-10 05:59:52 -0700

    fix

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,6 @@
-#ifndef _DOG_H
-#define _DOG_H
-int _putchar(char c);
+#ifndef DOG_H
+#define DOG_H
+
 /**
  * struct dog - defines a new type struct dog
  * @name: dog's name
@@ -18,4 +18,4 @@ void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
-#endif /**_DOG_H*/
+#endif /**DOG_H*/
